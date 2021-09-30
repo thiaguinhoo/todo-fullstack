dbf13b0587c0246771c44261a51e5dfe469a4c65
thiaguinhoo <thiagoluizsog@gmail.com>
2021-09-30 08:45:42 -0300

    added middlewares

--- backend/src/application.js
+++ backend/src/application.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const compression = require('compression');
+const helmet = require('helmet');
+const cors = require('cors');
 
 const application = express();
+
+application.use(express.json());
+application.use(express.urlencoded({ extended: true }));
+application.use(compression());
+application.use(helmet());
+application.use(cors());
+if (process.env.NODE_ENV == 'development') {
+    application.use(require('morgan')('tiny'));
+}
 
 module.exports = application;
